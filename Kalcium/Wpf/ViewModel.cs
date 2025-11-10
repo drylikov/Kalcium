8247acd077b05fdca30deb76cdfa5c11058cfe05
drylikov <denis.rylikov@protonmail.com>
2025-11-10 12:31:46 +0300

    Kalcium
    A library that provides powerful helpers, base classes, controls and tools. A must have if developing using WPF, MVVM, Linq and vector math .

---
+++ Kalcium/Wpf/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Kalcium.Wpf
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(params string[] properties)
+        {
+            foreach (var property in properties)
+                PropertyChanged.Raise(property, this);
+        }
+
+        public void SetProperty<T>(string propertyName, ref T field, T value)
+        {
+            if (object.Equals(field, value)) return;
+
+            field = value;
+            RaisePropertyChanged(propertyName);
+        }
+    }
+}
